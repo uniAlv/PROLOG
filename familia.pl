654df3b637a3b0b768ac3c4e0fdd478ab8a01720
Alvaro Medrano <aljak1815@gmail.com>
2024-02-27 21:26:48 -0400

    ejercicio familia

---
+++ familia.pl
@@ -0,0 +1,39 @@
+# cristiano.
+# mariel.
+# juan.
+# batista.
+# cristal.
+# mario.
+# alejandra.
+# adrian.
+# luciano.
+# alvaro.
+# mario.
+
+# ESTRUCTURA
+# Functor [esPadre] 
+# parametro [(maria)]
+
+esHombre(cristiano).
+esHombre(juan).
+esHombre(batista).
+esHombre(adrian).
+esHombre(luciano).
+esHombre(mario).
+esHombre(alvaro).
+
+esMujer(mariel).
+esMujer(cristal).
+esMujer(alejandra).
+
+esHijoHija(cristiano,batista).
+esHijoHija(mariel,cristal).
+esHijoHija(batista,adrian).
+esHijoHija(cristal,adrian).
+esHijoHija(batista,luciano).
+esHijoHija(cristal,luciano).
+esHijoHija(batista,alejandra).
+esHijoHija(cristal,alejandra).
+
+esHijoHija(alejandra,alvaro).
+esHijoHija(mario,alvaro).
