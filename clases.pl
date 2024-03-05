a1fac6b22d9fb8c929849636391aad12548fe53c
Alvaro Medrano <aljak1815@gmail.com>
2024-03-05 19:45:52 -0400

    esto es las clases

---
+++ clases.pl
@@ -0,0 +1,30 @@
+#atomo
+juan.
+mario.
+estela.
+'base de datos ii'.
+'redes i'.
+'inteligencia artificial'.
+arturo.
+andres.
+jose.
+karina.
+mariel.
+#enseña
+ensena(juan,'base de datos ii').
+ensena(estela,'redes i').
+ensena(estela,'inteligencia artificial').
+ensena(maria,'base de datos ii').
+ensena(maria,'redes i').
+ensena(maria,'inteligencia artificial').
+#asiste
+asiste(arturo,'redes i').
+asiste(arturo,'base de datos ii').
+asiste(andres,'redes i').
+asiste(andres,'inteligencia artificial').
+asiste(jose,'redes i').
+asiste(jose,'base de datos ii').
+asiste(jose,'inteligencia artificial').
+asiste(karina,'redes i').
+asiste(karina,'inteligencia artificial').
+asiste(mariel,'inteligencia artificial').
