0be739a29c4f29e997d724eb92283b2a4f6a6b68
Alvaro Medrano <aljak1815@gmail.com>
2024-03-12 19:50:45 -0400

    ;-; reglas añadidas

--- familiaDioses.pl
+++ familiaDioses.pl
@@ -46,6 +46,7 @@ esDecendienteDirecto(hermes,atena).
 
 %reglas
 %padre
+padreRegla(Padre,Hijo):-esDecendienteDirecto(Hijo,Padre),esHombre(Padre).
 esPadre(cronos,hera).
 esPadre(ares,afrodita).
 esPadre(ares,hefesto).
@@ -54,6 +55,7 @@ esPadre(hefesto,atena).
 esPadre(hefesto,baco).
 
 %madre
+madreRegla(Madre,Hijo):-esDecendienteDirecto(Hijo,Madre),esHombre(Madre).
 esMadre(hera,afrodita).
 esMadre(hera,hefesto).
 esMadre(afrodita,zeus).
@@ -62,6 +64,7 @@ esMadre(afrodita,atena).
 esMadre(atena,hermes).
 
 %hijo
+hijoRegla(Hijo,Padre):- esHombre(Hijo),esDecendienteDirecto(Hijo,Padre).
 esHijo(hefesto,hera).
 esHijo(hefesto,ares).
 esHijo(zeus,afrodita).
@@ -71,6 +74,7 @@ esHijo(baco,hefesto).
 esHijo(hermes,atena).
 
 %hija
+hijaRegla(Hijo,Padre):- esMujer(Hijo),esDecendienteDirecto(Hijo,Padre).
 esHija(hera,cronos).
 esHija(afrodita,hera).
 esHija(afrodita,ares).
@@ -79,17 +83,20 @@ esHija(nera,zeus).
 esHija(nera,hipolita).
 
 %hermano
+hermanoRegla(HermanoA,HermanoB):-esDecendienteDirecto(HermanoA,Padre),esHombre(Hermano).
 esHermano(hefesto,afrodita).
 esHermano(baco, atena).
 esHermano(zeus,baco).
 esHermano(baco,zeus).
 
 %hermana
+hermanaRegla(HermanaA,HermanaB):-esDecendienteDirecto(HermanaA,Padre),esHombre(Hermana).
 esHermana(afrodita,hefesto).
 esHermana(atena,baco).
 esHermana(atena,zeus).
 
 %abuelo
+abueloRegla(Abuelo,Nieto):-esDecendienteDirecto(Nieto,Padre),esDecendienteDirecto(Padre,Abuelo),esHombre(Abuelo)
 esAbuelo(cronos,hefesto).
 esAbuelo(cronos,afrodita).
 esAbuelo(ares,atena).
@@ -99,6 +106,7 @@ esAbuelo(poseidon,nera).
 esAbuelo(hefesto,hermes).
 
 %abuela
+abuelaRegla(Abuela,Nieto):-esDecendienteDirecto(Nieto,Padre),esDecendienteDirecto(Padre,Abuela),esHombre(Abuela)
 esAbuela(hera, atena).
 esAbuela(hera, baco).
 esAbuela(hera, zeus).
